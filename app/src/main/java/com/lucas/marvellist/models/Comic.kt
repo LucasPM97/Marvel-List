d473f9061af5c25166066f58a195edb31885bda7
Lucas <lucasmontanari.97@gmail.com>
2021-04-14 17:05:51 -0300

    Fix Comic item UI

--- app/src/main/java/com/lucas/marvellist/models/Comic.kt
+++ app/src/main/java/com/lucas/marvellist/models/Comic.kt
@@ -2,20 +2,29 @@ package com.lucas.marvellist.models
 
 import android.os.Parcel
 import android.os.Parcelable
+import java.lang.Exception
 
 data class Comic(
-    val title: String? = "",
-    val year: String? = ""
+    val name: String? = "",
 ) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString()
-    ) {
+
+    fun getComicYear(): String {
+
+        try {
+            if (name.isNullOrEmpty()) return ""
+
+            return name.substring(name.indexOf("(") + 1, name.indexOf(")"))
+        } catch (ex: Exception) {
+            println(ex)
+            return ""
+        }
+    }
+
+    constructor(parcel: Parcel) : this(parcel.readString()) {
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(title)
-        parcel.writeString(year)
+        parcel.writeString(name)
     }
 
     override fun describeContents(): Int {
