cf002390c2c18e2b7bfed73eaea8d7d071f728d9
Lucas <lucasmontanari.97@gmail.com>
2021-04-15 16:48:09 -0300

    Fix Event Start/End Date format

--- app/src/main/java/com/lucas/marvellist/models/Event.kt
+++ app/src/main/java/com/lucas/marvellist/models/Event.kt
@@ -1,20 +1,23 @@
 package com.lucas.marvellist.models
 
+import com.lucas.marvellist.utils.extensions.*
+
 data class Event(
     val title: String,
-    private val start: String,
-    private val end: String,
+    private val start: String?,
+    private val end: String?,
     val thumbnail: ImageThumbnail? = null,
 ) {
     fun getStartDate(): String {
-        return start
+        return start?.toSimplifiedDateString() ?: ""
     }
 
     fun getEndDate(): String {
-        return end
+        return end?.toSimplifiedDateString() ?: ""
     }
 
     fun getSmallImageUrl(): String {
         return thumbnail?.getSmallImageUrl() ?: ""
     }
 }
+
