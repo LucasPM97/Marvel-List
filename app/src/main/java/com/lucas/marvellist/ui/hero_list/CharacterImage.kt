22721f9d6d088a22d9b0eae91b5a7b627aa6bd61
Lucas Perera Montanari <lucasmontanari.97@gmail.com>
2021-08-15 21:25:39 -0300

    Add Image loaded with Glide

---
+++ app/src/main/java/com/lucas/marvellist/ui/hero_list/CharacterImage.kt
@@ -0,0 +1,35 @@
+package com.lucas.marvellist.ui.hero_list
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import com.lucas.marvellist.R
+import com.lucas.marvellist.models.Character
+import com.lucas.marvellist.ui.composables.loadPicture
+
+@Composable
+fun CharacterImage(character : Character, modifier : Modifier){
+    val imageState = loadPicture(character.getSmallImageUrl())
+    imageState.value?.let{bitmap ->
+        Image(
+            bitmap = bitmap.asImageBitmap(),
+            contentDescription = "${character.name} image",
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .background(
+                    colorResource(R.color.image_background_color),
+                    RoundedCornerShape(
+                        topStart = dimensionResource(R.dimen.small_image_corner)
+                    )
+                )
+            )
+    }
+}
--- app/src/main/java/com/lucas/marvellist/ui/hero_list/HeroItem.kt
+++ app/src/main/java/com/lucas/marvellist/ui/hero_list/HeroItem.kt
@@ -1,26 +1,14 @@
 package com.lucas.marvellist.ui.hero_list
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lucas.marvellist.models.Character
-import com.lucas.marvellist.R
-import com.lucas.marvellist.utils.extensions.fontDimensionResource
 
 
 @Composable
@@ -37,40 +25,16 @@ fun HeroItem(character: Character, modifier: Modifier, onClick: (Character) -> U
             shape = RoundedCornerShape(4.dp)
         ) {
             Row {
-                Image(
-                    painter = painterResource(R.drawable.stan_lee),
-                    contentDescription = "${character.name} image",
-                    contentScale = ContentScale.Crop,
+                CharacterImage(
+                    character,
                     modifier = Modifier
                         .size(120.dp)
-                        .background(
-                            colorResource(R.color.image_background_color),
-                            RoundedCornerShape(
-                                topStart = dimensionResource(R.dimen.small_image_corner)
-                            )
-                        )
                 )
-                Column(
+                CharacterInfo(
+                    character,
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(start = 16.dp, end = 30.dp, bottom = 28.dp)
-                ) {
-                    Text(
-                        (character.name ?: "").uppercase(),
-                        fontSize = fontDimensionResource(R.dimen.title_font_size),
-                        color = colorResource(R.color.black),
-                        fontFamily = FontFamily.SansSerif
-                    )
-                    Spacer(modifier = Modifier.height(9.dp))
-                    Text(
-                        character.description ?: "",
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis,
-                        color = colorResource(R.color.description_text_color),
-                        fontSize = fontDimensionResource(R.dimen.description_font_size)
-                    )
-                }
-
+                        .padding(start = 16.dp, end = 30.dp, bottom = 28.dp))
             }
 
         }
